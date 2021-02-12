77ab351068762414f3b5ea55e7bad7e1349a40d8
leindv <leindv@yahoo.com>
2021-02-12 11:38:24 +0500

    Ch. 2 F-string Ex. 3

--- full_name.py
+++ full_name.py
@@ -1,6 +1,11 @@
 # Здесь f-строка (от слова "format"). Формирует строку, заменяя имена 
 # переменных в фигурных скобках {} их значениями.
+# Эти строки впервые появились в python 3.6
+# Более ранних версиях вместо f используем метод format()
+# Например, ful_mame = "{} {}".format(first_name, last_name)
+# 
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(f"Hello, {full_name.title()}!")
+massage = f"Hello, {full_name.title()}!"
+print(massage)
