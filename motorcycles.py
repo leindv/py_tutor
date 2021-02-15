9477348fb7b34432377fc27e86838ef7f693a2ea
lein_d_v <leindv@yahoo.com>
2021-02-15 11:52:18 +0500

    Ch. 3 Modifying a list item Ex. 7

---
+++ motorcycles.py
@@ -0,0 +1,6 @@
+# Изменение элементов в списке
+# Прога изменяет первый [0] элемент списка
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
