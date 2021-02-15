312c3acf241c074d5d4bbca37e76c08d019a8566
lein_d_v <leindv@yahoo.com>
2021-02-15 16:24:39 +0500

    Ch. 4 cycle FOR with f-string Ex. 4

--- magicians.py
+++ magicians.py
@@ -1,7 +1,8 @@
 # С использованием f-строки.
-#
+# Сообщение 1 будет выведено один раз - она вне цикла!
 #
 magicians = ['alice', 'david', 'corolina'] 
 for magician in magicians: 
     print(f"{magician.title()}, that was a great trick!")
     print(f"I can not wait to see your next track, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!") #1
