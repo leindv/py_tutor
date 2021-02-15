9783a30cbd8a26c5c8a1757c35258999c022b9ea
lein_d_v <leindv@yahoo.com>
2021-02-15 16:20:39 +0500

    Ch. 4 cycle FOR with f-string Ex. 3

--- magicians.py
+++ magicians.py
@@ -4,4 +4,4 @@
 magicians = ['alice', 'david', 'corolina'] 
 for magician in magicians: 
     print(f"{magician.title()}, that was a great trick!")
-    
+    print(f"I can not wait to see your next track, {magician.title()}.\n")
