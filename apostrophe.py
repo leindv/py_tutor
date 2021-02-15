59163aee24469ab092f83c7923a620c660b50cd6
leindv <leindv@yahoo.com>
2021-02-15 10:14:40 +0500

    Ch. 2 synt_err Ex. 2

--- apostrophe.py
+++ apostrophe.py
@@ -1,2 +1,6 @@
-massage = "One of Python's strenghts is its diverse community."
+# При использовании одинарных ковычек '' в строке massage
+# бвыскочит синтаксическая ошибка, так как в строке есть 
+# апостров в слове Python's. 
+#
+massage = 'One of Python's strenghts is its diverse community.'
 print (massage)
