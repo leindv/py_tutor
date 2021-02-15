a2405be198ffb7293558303cc803f7ee635c7474
lein_d_v <leindv@yahoo.com>
2021-02-15 15:37:53 +0500

    Ch. 3 sorted-method Ex. 19

--- cars.py
+++ cars.py
@@ -1,7 +1,14 @@
-# Постоянная сортировка списка методом sort()
-# Используя аргумент reverse=True сортируем
-# элементы в списке в обратном алфавитному
-# порядке
+# Метод sorted() применяется для 
+# сохранения исходного порядка элементов списка,
+# но временного предсавления его в отсортированном 
+# виде.
+# Сначала список отобразиться в исходном (1) порядке,
+# затем он отсортируется в алфавитном (2) и вернется в 
+# исходное (3) состояние
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort(reverse=True) 
+print("Here is the original list:") #1
+print(cars)
+print("\nHere is the sorted list:") #2
+print(sorted(cars))
+print("\nHere is the original list againe:") #3
 print(cars)
