c5ea7ccf453c558872f4b4dabb0a9e859c1af942
leindv <leindv@yahoo.com>
2021-02-15 11:10:36 +0500

    Ch. 3 Ex. 3

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,5 @@
+# Используя метод title() выведем первый (начинается с нуля)
+# элемент списка.
+# Кстати, в vscode прога (все три примера) заработала!!!!
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[0])
+print(bicycles[0].title())
