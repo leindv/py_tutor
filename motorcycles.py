bef74f2eb6854f6d4726204bf62e74c8f718363d
lein_d_v <leindv@yahoo.com>
2021-02-15 14:44:04 +0500

    Ch. 2 Delete remove-metod Ex. 15

--- motorcycles.py
+++ motorcycles.py
@@ -1,8 +1,7 @@
-# Пример с использованием f-строки и метода pop()
-# для удаления элемента в произвольной позиции.
-# С начала первый элемент извлекается из точки 1.
-# Затем в точке 2 выводится сообщение об этом мотоцикле.
-#
-motorcycles = ['honda', 'yamaha', 'suzuki'] 
-first_owned = motorcycles.pop(0) #1
-print(f"The first motorcycle I owned was a {first_owned.title()}.") #2
+# Удаление элементов по значению.
+# Применяется когда позиция удаляемого значения неизвестна
+# Используется метод remove()
+motorcycles = ['honda', 'yamaha', 'suzuki', 'dukati']
+print(motorcycles)
+motorcycles.remove('dukati')
+print(motorcycles)
