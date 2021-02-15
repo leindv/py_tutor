5b9de51d59f7690a3051303af7463508b723e404
lein_d_v <leindv@yahoo.com>
2021-02-15 14:39:31 +0500

    Ch. 3 Delete pop-method with f-string Ex.14

--- motorcycles.py
+++ motorcycles.py
@@ -1,4 +1,8 @@
 # Пример с использованием f-строки и метода pop()
-motorcycles = ['honda', 'yamaha', 'suzuki']
-last_owned = motorcycles.pop()
-print(f"The last motorcycle I owned was a {last_owned.title()}.")
+# для удаления элемента в произвольной позиции.
+# С начала первый элемент извлекается из точки 1.
+# Затем в точке 2 выводится сообщение об этом мотоцикле.
+#
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+first_owned = motorcycles.pop(0) #1
+print(f"The first motorcycle I owned was a {first_owned.title()}.") #2
