e3376258a2c5081ff02d1a787f4bb7e8f013d825
lein_d_v <leindv@yahoo.com>
2021-02-15 15:42:37 +0500

    Ch. 3 reverse-method Ex. 20

--- cars.py
+++ cars.py
@@ -1,14 +1,7 @@
-# Метод sorted() применяется для 
-# сохранения исходного порядка элементов списка,
-# но временного предсавления его в отсортированном 
-# виде.
-# Сначала список отобразиться в исходном (1) порядке,
-# затем он отсортируется в алфавитном (2) и вернется в 
-# исходное (3) состояние
+# Метод reverse() применяется для 
+# сортировки списка в обратном (НЕ в АЛФАВИТНОМ)
+# 
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-print("Here is the original list:") #1
 print(cars)
-print("\nHere is the sorted list:") #2
-print(sorted(cars))
-print("\nHere is the original list againe:") #3
+cars.reverse()
 print(cars)
