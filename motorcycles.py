41375c547198e1bd4d60e52d384b9350f84c5e8e
lein_d_v <leindv@yahoo.com>
2021-02-15 15:56:51 +0500

    Ch. 3 Error Index List Ex. 22

--- motorcycles.py
+++ motorcycles.py
@@ -1,13 +1,3 @@
-# Удаление элементов по значению методом remove()
-# с выводом причин удаления (Здесь применяется f-строка)
-# После определения списка в точке 1, значение элемента списка сох-
-# раняется в точке 2. Затем эта переменная сообщает Python,какое 
-# значение должно быть удалено из списка (3). 
-# В точке 4 значение элемента было удалено из списка,
-# но продолжает храниться в переменной  too_expensive.
-motorcycles = ['honda', 'yamaha', 'suzuki', 'dukati'] #1
-print(motorcycles)
-too_expensive = 'dukati' #2
-motorcycles.remove(too_expensive) #3
-print(motorcycles)
-print(f"\nA {too_expensive.title()} is too expensive for me.") #4
+# Ошибка индексирования при работе со списком
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+print(motorcycles[3])
