d563d50e2bbd55051abe9fb35c97ff6068b4fb9b
lein_d_v <leindv@yahoo.com>
2021-02-15 14:58:45 +0500

    Ch. 3 Delete remove-method with f-string Ex.16

--- motorcycles.py
+++ motorcycles.py
@@ -1,7 +1,13 @@
-# Удаление элементов по значению.
-# Применяется когда позиция удаляемого значения неизвестна
-# Используется метод remove()
-motorcycles = ['honda', 'yamaha', 'suzuki', 'dukati']
+# Удаление элементов по значению методом remove()
+# с выводом причин удаления (Здесь применяется f-строка)
+# После определения списка в точке 1, значение элемента списка сох-
+# раняется в точке 2. Затем эта переменная сообщает Python,какое 
+# значение должно быть удалено из списка (3). 
+# В точке 4 значение элемента было удалено из списка,
+# но продолжает храниться в переменной  too_expensive.
+motorcycles = ['honda', 'yamaha', 'suzuki', 'dukati'] #1
 print(motorcycles)
-motorcycles.remove('dukati')
+too_expensive = 'dukati' #2
+motorcycles.remove(too_expensive) #3
 print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.") #4
