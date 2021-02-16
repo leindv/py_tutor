ba715c7096d5d118df2066a88ff923559e0071f1
lein_d_v <leindv@yahoo.com>
2021-02-16 17:14:53 +0500

    p. 98 file: toppings.py Ex. 1

--- toppings.py
+++ toppings.py
@@ -1,9 +1,11 @@
-# Строка 1 сравнивает значение requested_topping 
-# со значением 'anchovies'. Если эти два значения 
-# НЕ равны, то возвращается True, и выполняется 
-# код if. Если эти два значения равны, 
-# прога возвращает False и не выполняет код 
-# после команды if.  
-requested_topping = 'mashrooms'
-if requested_topping != 'anchovies': #1
-    print('Hold the anchovies')
+# Обработка начинается в строке 1 со списка, содержащего 
+# топпинги. Команды в точке 2 проверяют , включает ли заказ 
+# конкретные топпинги итд итп etc.... 
+requested_toppings = ['mashrooms', 'extra cheese'] #1
+if 'mashrooms' in requested_toppings: #2
+    print("Adding mashrooms.")
+if 'pepperoni' in requested_toppings: #3
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings: #4
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")       
