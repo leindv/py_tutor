30197be7064be80799b75d1c9f0a378b1d1440c9
lein_d_v <leindv@yahoo.com>
2021-02-16 11:44:55 +0500

    p. 78 file: foods.py  Ex.2

--- foods.py
+++ foods.py
@@ -1,8 +1,10 @@
 # Для копирования списка используется конструкция [:]
 # В точке 1 создается список элементов с именем my_foods.
 # В 2 создается другой список.
-my_foods = ['pizza', 'falafel', 'carrot cake']
-friend_foods = my_foods[:]
+my_foods = ['pizza', 'falafel', 'carrot cake'] #1
+friend_foods = my_foods[:] #2
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
 print("My favorite foods are:")
 print(my_foods)
 print("\nMy friend's favorite foods are:")
