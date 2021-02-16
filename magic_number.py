04d2d7028e045e19d4568746fedf3c1355fa5053
lein_d_v <leindv@yahoo.com>
2021-02-16 15:50:23 +0500

    p. 89 file: magic_numder.py Ex. 1

---
+++ magic_number.py
@@ -0,0 +1,7 @@
+# Аналогичнофайлу из предыдущего коммита. 
+# Условие answer (17) не равго 42. Так как условие 
+# истино - блок с отступом выполняется! 
+#
+answer = 17
+if answer != 42: #1
+    print("That is not correct answer.Please tre again!")
