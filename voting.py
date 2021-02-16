126ff671541b2bb6c52461aa0472de0b44f178bb
lein_d_v <leindv@yahoo.com>
2021-02-16 16:23:11 +0500

    p. 94 file: voting.py Ex. 3

--- voting.py
+++ voting.py
@@ -1,7 +1,11 @@
-# Аналогично предыдущему коммиту. Если 
-# условие истинно, то выполняются все 
-# условия.
-age = 19 
-if age >= 18:
+# Если условие 1 истинно, то выполняется первый блок
+# с командами print. Если же ложно - выполняется блок 
+# else в точке 2.
+age = 17 
+if age >= 18: #1
     print("You are old enoug to votr!")
     print("Have you registered to vote yet?")
+else: #2
+    print("Sorry, you are too young to vote.")
+    print("Please regisrter to vote as soon as turn 18!")
+
