1bbc37d75f4d067761a82d74b42affe0a22a32cf
lein_d_v <leindv@yahoo.com>
2021-02-16 11:52:55 +0500

    p. 79 file: foods.py  Ex.3

--- foods.py
+++ foods.py
@@ -1,8 +1,9 @@
 # Для копирования списка используется конструкция [:]
-# В точке 1 создается список элементов с именем my_foods.
-# В 2 создается другой список.
-my_foods = ['pizza', 'falafel', 'carrot cake'] #1
-friend_foods = my_foods[:] #2
+# Здесь данной конструкции (1) нет, поэтому обе переменные 
+# связаны одним списком. Т.о. элемент "ice cream" появиться 
+# в обеих списках. 
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods #1
 my_foods.append('cannoli')
 friend_foods.append('ice cream')
 print("My favorite foods are:")
