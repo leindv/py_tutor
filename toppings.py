b2cd9ec8fdf87fbf2ace6f7591f56e72edb5f372
lein_d_v <leindv@yahoo.com>
2021-02-16 15:41:07 +0500

    p. 89 file: tohhings.py Ex. 1

---
+++ toppings.py
@@ -0,0 +1,9 @@
+# Строка 1 сравнивает значение requested_topping 
+# со значением 'anchovies'. Если эти два значения 
+# НЕ равны, то возвращается True, и выполняется 
+# код if. Если эти два значения равны, 
+# прога возвращает False и не выполняет код 
+# после команды if.  
+requested_topping = 'mashrooms'
+if requested_topping != 'anchovies': #1
+    print('Hold the anchovies')
