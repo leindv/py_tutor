70a9a9965cf86f45811bbdb06adf10e6cf66fb50
lein_d_v <leindv@yahoo.com>
2021-02-16 12:22:55 +0500

    TUPLES p. 82 file: dimensions.py with FOR  Ex.4

--- dimensions.py
+++ dimensions.py
@@ -1,8 +1,16 @@
 # Кортежи (TUPLES) - это набор элемеентов, 
 # значение которых не изменяется.
-# Обозначаются кортежи ().
-# Перебор всез значений кортежа
+# Но мы можем присвоить НОВОЕ значение переменной, 
+# в которой храниться кортеж.
+# В точке 1 определяем исходный кортеж, в 2 
+# сохраняется новый кортеж, после чего выводятся 
+# новые значения (3)
 #
-dimensions = (200, 50)
+dimensions = (200, 50) #1
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+dimensions = (400, 100) #2
+print("\nModified dimensions:") #3
 for dimension in dimensions:
     print(dimension)
