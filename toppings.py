be569b1a16b83d23eb641ac3b40206198be090c8
lein_d_v <leindv@yahoo.com>
2021-02-17 11:53:25 +0500

    p. 100 file: toppings.py Ex. 3

--- toppings.py
+++ toppings.py
@@ -1,11 +1,11 @@
-# В отличии от предыдущего коммита здесь первое значение 
-# является истинным, поэтому остальные значения 
-# не проверяются.
+# 
+# Приводим список как на примере программным 
+# способом с проверкой через print.
 requested_toppings = ['mashrooms', 'extra cheese'] 
-if 'mashrooms' in requested_toppings: 
-    print("Adding mashrooms.")
-elif 'pepperoni' in requested_toppings: 
-    print("Adding pepperoni.")
-elif 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+requested_toppings.insert(1, 'green peppers')
+print(requested_toppings)
+
+# Выполнение учебного примера.
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
 print("\nFinished making your pizza!")       
