082f97fa5a7f9abc179176b4f8c7c4f5ea35266d
lein_d_v <leindv@yahoo.com>
2021-02-17 12:02:40 +0500

    p. 101 file: toppings.py Ex. 4

--- toppings.py
+++ toppings.py
@@ -5,7 +5,16 @@ requested_toppings = ['mashrooms', 'extra cheese']
 requested_toppings.insert(1, 'green peppers')
 print(requested_toppings)
 
-# Выполнение учебного примера.
+# Выполнение учебного примера. Здесь прога 
+# проверяет каждый элемент списка. В точке 1 
+# прога проверяет заказал ли клиент "зеленый 
+# перец" и если "да" - выводит сообщение об 
+# его отсутсвии. Блок в точке 2 гарантирует, 
+# что другие дополнения списка будут включены.
 for requested_topping in requested_toppings:
-    print(f"Adding {requested_topping}.")
+    if requested_topping == 'green peppers': #1
+        print('Sorry? we are out of green peppers right now.')
+    else: #2
+        print(f"Adding {requested_topping}.")
+
 print("\nFinished making your pizza!")       
