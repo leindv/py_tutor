03015bce3b82debccdc3e82075a060910310f85b
lein_d_v <leindv@yahoo.com>
2021-02-20 11:26:24 +0500

    Ch. 7 file: parrot.py p. 128

---
+++ parrot.py
@@ -0,0 +1,5 @@
+# Программа предлагает ввести текст при помощи фунции INPUT(), и
+# выводит сообщение для пользователя.
+#
+massage = input("Tell me something, and I will repeat it back to you: ")
+print(massage)
