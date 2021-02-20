db927d346bc96f8e308a6bd1a8f2e617fb263ca5
lein_d_v <leindv@yahoo.com>
2021-02-20 08:43:45 +0500

    p. 119 file: favorite_languages.py Ex. 7

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,5 +1,9 @@
 # Перебор всех значений в словаре - применение метода
-# values().
+# values(). Возможны ситуации, когда значения в словаре 
+# дублируются, и необходимо в проге эти дубликаты исключить.
+# В этом случае применяется метод SET() - множество - этот 
+# метод исключает дублирование. В точке 1 set() используется 
+# для извлечения уникальных данных.
 #
 favorit_languages = {
     'jen': 'python',
@@ -8,5 +12,5 @@ favorit_languages = {
     'phil': 'python',
     }
 print("The following languages have been mentoned:")
-for language in favorit_languages.values():
+for language in set(favorit_languages.values()): #1
     print(language.title())
