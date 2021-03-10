aeb5f7203ce50a63efa8dca714bdb2713cbbab97
lein_d_v <leindv@yahoo.com>
2021-03-10 14:35:53 +0500

    p. 132 file: counting.py Ex. 1

---
+++ counting.py
@@ -0,0 +1,13 @@
+# В строке 1 отсчет начинается с 1, отчего current_number 
+# присваевается значение "1". Далее запускается цикл while (2), 
+# который продолжает работать показначение current_number 
+# останется меньше или равно 5. Код в цикле выводит значение 
+# current_number (3) и увеличивает его на 1 командой (4). 
+# Команда (4) аналогична команде (5.)
+#  
+current_number = 1 #1
+while current_number <= 5: #2
+    print(current_number) #3
+    current_number += 1 #4
+    #current_number = current_number + 1 #5
+    
