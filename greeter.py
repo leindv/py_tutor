c8e2007b478c9eb963956e16366ed80b21617259
lein_d_v <leindv@yahoo.com>
2021-03-10 11:09:59 +0500

    p. 129 file: greeter.py Ex.2

--- greeter.py
+++ greeter.py
@@ -1,6 +1,10 @@
-# При использовании функции input() надо включить точную подсказку 
-# пользователю от информации, которую надо ввести, с добавлением 
-# ПРОБЕЛА в конце полсказки. 
+#  Если подсказка занимает более одной строки,
+#  то текст подсказкт можно сохранить в переменной (1) 
+#  и пердать эту переменную в функцию input()(2).
+#  Здесь оператор += объединяет тескт.
 #
-name = input("Please enter your name: ")
+promt = "If you tell us you are, we can personalize thr massages you see."
+promt += "\nWhat is your first name? " #1
+
+name = input(promt) #2
 print(f"\nHello, {name}!")
