eaed5a76aba95c0d34cb1aa964c21279c82f8496
lein_d_v <leindv@yahoo.com>
2021-03-10 14:50:35 +0500

    p. 132 file: parrot.py Ex. 1

--- parrot.py
+++ parrot.py
@@ -1,5 +1,14 @@
-# Программа предлагает ввести текст при помощи фунции INPUT(), и
-# выводит сообщение для пользователя.
+# В точке 1 определяется сообщение, которое объясняет, 
+# что у пользователя есть два варианта: ввести сообщение 
+# или ввести признак завершения (строка 'quit'). Затем в 
+# переменной (2) присваивается введенное пользователем значение. 
+# В проге эта переменная инициализируется пустой строкой " ". 
+# Цикл будет выполнятся пока значение massage НЕ РАВНО 'quit'.
 #
-massage = input("Tell me something, and I will repeat it back to you: ")
-print(massage)
+prompt = "\nTell me something, and I will repeat it back to you:" #1
+prompt += "\nEnter 'quit' to end the program."
+
+massage = "" #2
+while massage != 'quit': #3
+    massage = input(prompt)
+    print(massage)
