dfc38d605a78d4dcc03dd3607e112f8626d5174e
lein_d_v <leindv@yahoo.com>
2021-03-10 14:58:47 +0500

    p. 134 file: parrot.py Ex. 2

--- parrot.py
+++ parrot.py
@@ -1,14 +1,12 @@
-# В точке 1 определяется сообщение, которое объясняет, 
-# что у пользователя есть два варианта: ввести сообщение 
-# или ввести признак завершения (строка 'quit'). Затем в 
-# переменной (2) присваивается введенное пользователем значение. 
-# В проге эта переменная инициализируется пустой строкой " ". 
-# Цикл будет выполнятся пока значение massage НЕ РАВНО 'quit'.
+# Дополнили прогу из предыдущего коммита с целью не выводить 
+# сообщение 'quit', если оно вводится пользователем. Решается 
+# проверкой IF (1).
 #
-prompt = "\nTell me something, and I will repeat it back to you:" #1
-prompt += "\nEnter 'quit' to end the program."
+prompt = "\nTell me something, and I will repeat it back to you:" 
+prompt += "\nEnter 'quit' to end the program. "
 
-massage = "" #2
-while massage != 'quit': #3
+massage = " " 
+while massage != 'quit':
     massage = input(prompt)
-    print(massage)
+    if massage != 'quit': #1
+        print(massage)
