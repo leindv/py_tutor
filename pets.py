80efdcef0f5017784db998aacb5bb102c0c6550b
lein_d_v <leindv@yahoo.com>
2021-03-15 14:59:00 +0500

    p. 147 file: pets.py Ex. 3

--- pets.py
+++ pets.py
@@ -1,10 +1,10 @@
-# Функция describe_pet может вызываться в программе несколько 
-# раз. Для вызова инфо о другом животном достаточно одного 
-# вызова функции describe_pet().
-# 
+# ИМЕННОВАННЫЕ АРГУМЕНТЫ состоят из имени параметров и 
+# их значений, передаваемых функции. 
+# В отличии от ПОЗИЦИОННЫХ аргументов порядок следования 
+# ИМЕННОВАНЫХ АРГУМЕНТОВ не важен (1).
 def describe_pet(animal_type, pet_name): 
     """Выводит информацию о животном."""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
-describe_pet('hamster', 'harry')
-describe_pet('dog', 'willie')
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster') #1
