1feecb23f64e87ca4d2f40a32858d78498461952
lein_d_v <leindv@yahoo.com>
2021-03-16 16:14:39 +0500

    p. 161 file: pizza.py Ex. 3

--- pizza.py
+++ pizza.py
@@ -1,16 +1,14 @@
-# Передача произвольного набора аргументов.
-# Звездочка с именем параметра (*toppings) приказывает проге
-# создать пустой кортеж с именем toppings и упаковать в него 
-# все полученные значения.
-# Здесь команду print из предыдущего коммита заменяем циклом,
-# котроый перебирает список топпингов и выводит описание заказанной 
-# пиццы.
+# Позиционные аргументы с призвольным набором аргументов.
+# Если надо, чтобы функция могла вызываться с различным
+# количеством аргументов, параметр для получения произвольного 
+# количества аргументов долженстоятьна ПОСЛЕДНЕМ месте в определении 
+# функции.
 
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Выводит список заказанных топпиноговю"""
-    print("\nMaking a pizza with the following toppings:")
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
