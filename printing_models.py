09b7d3b2f27a2b9f10a461740a0dae748fcc1558
lein_d_v <leindv@yahoo.com>
2021-03-16 15:53:07 +0500

    p. 159 (NB-NB-NB) file: prinring_mpdels.py Ex. 2

--- printing_models.py
+++ printing_models.py
@@ -1,26 +1,33 @@
-# Изменение списка в функции.
-# В начале проги создается список моделей и пустой список, в 
-# который каждая модель перемещается после печати.
-# Пока в unprinted_designs остаются модели цикл while имитирует
-# печать каждой модели: модель удаляется из конца списка и 
-# сохраняется в current_design, а пользователь получает сообщение
-# о том, что текущая модель была напечатана.Затем модель перемещается 
-# в список напечатанных. После завершения цикла выводится список 
-# напечатанных моделей.  
-#
+# ИЗМЕНЕНИЕ СПИСКА В ФУНКЦИИ.
+# Меняем структуру кода от предыдущего коммита.  
+# В точке (1) опеделяется функция с двумя папаметрами, она 
+# имитирует печать каждой модели, последовательноизвлекая 
+# модели из первого списка и перемещая их во второй список.
+# В точке (2) определяется функция с одним параметром - 
+# список напечатанных моделей Эта функция получает этот 
+# список и выводит имена всех напечатанных моделей.
+# Программа создает список моделей для печати и пустой список 
+# для готовых моделей(3). Затем, так как обе функции определены,
+# остается ВЫЗВАТЬ их и передать правильные АРГУМЕНТЫ (4).
 
-# Список моделей, которые надо напечатать.
-unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+def print_models(unprinted_designs, completed_models): #1
+    """
+    Имитирует печать моделей, пока список не станет пустым.
+    Каждая модель после печати перемещается в completed_models.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+        
+def show_completed_models(completed_models): #2
+    """Выводи инфо о всех напечатанных моделях."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+        
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron'] #3
 completed_models = []
 
-# Цикл последовательно печатает каждую модель до конца списка.
-# после печати каждая модель перемещается в список completed_models.
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
-
-# Вывод всех готовых моделей.
-print("\nThe following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
+print_models(unprinted_designs, completed_models) #4
+show_completed_models(completed_models)
