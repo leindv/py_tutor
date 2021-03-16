066f0f47d95939c199c0add0f4d053ece34d6400
lein_d_v <leindv@yahoo.com>
2021-03-16 14:33:46 +0500

    p. 155 (NB) file: greeter.py Ex. 1

--- greeter.py
+++ greeter.py
@@ -1,10 +1,21 @@
-#  Передача информации ФУНКЦИИ.
-#  Переменная username в определение функции 
-#  называется ПАРАМЕТР. Значение 'jesse'  - АРГУМЕНТ,
-#  то есть конкретная информация, переданная при вызове функции.
-#  
-def greet_user(username): 
-    """Выводит простое приветствие.""" 
-    print(f"Hello, {username.title()}!") 
+#  Использование функции в цикле WHILE.
+#  Здесь имя и фамилия в цикле запрашивается отдельно.
+def get_formatted_name(first_name, last_name):
+    """Возвращает аккуратно отформатированныое полное имя."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-greet_user('jesse') 
+while True: # Бесконечный цикл, который...
+    print("\nPlease tell me your name:") #1
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break #...прерывается если условие ЛОЖНО
+
+    l_name = input("Last neme: ")
+    if l_name == 'q':
+        break #... здесь аналогично...
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+    
