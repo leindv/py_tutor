2a86192c54a175be3cc9379af2d22f3910de4044
lein_d_v <leindv@yahoo.com>
2021-03-16 16:08:09 +0500

    p. 161 (NB)file: pizza.py Ex. 2

--- pizza.py
+++ pizza.py
@@ -2,10 +2,15 @@
 # Звездочка с именем параметра (*toppings) приказывает проге
 # создать пустой кортеж с именем toppings и упаковать в него 
 # все полученные значения.
+# Здесь команду print из предыдущего коммита заменяем циклом,
+# котроый перебирает список топпингов и выводит описание заказанной 
+# пиццы.
 
 def make_pizza(*toppings):
     """Выводит список заказанных топпиноговю"""
-    print(toppings)
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
 
 make_pizza('pepperoni')
 make_pizza('mushrooms', 'green peppers', 'extra cheese')
