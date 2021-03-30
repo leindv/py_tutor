093f590ff233102d2286f1943dce7011242bef2b
LeinDV <leindv@gmail.com>
2021-03-30 21:20:50 +0500

    p. 188 CLASS IMOORT file: car.py Ex. 2

---
+++ my_electric_car.py
@@ -0,0 +1,9 @@
+
+
+from car import ElectricalCar
+
+my_tesla = ElectricalCar('tesla', 'model s', 2019)
+
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
