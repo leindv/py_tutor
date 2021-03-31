8beba081dd3b19619471736dbdef66d687830370
lein_d_v <leindv@yahoo.com>
2021-03-31 10:27:32 +0500

    p. 190 CLASS IMPORT file: my_car.py Ex. 2

--- my_car.py
+++ my_car.py
@@ -1,11 +1,18 @@
-# Импортирование одного класса.
+# Импортирование нескольких классов из модуля.
 
-# Команда import в (1) приказывает проге 
-# открыть модуль car и импортировать 
-# класс Car().
+# Импортирование нескольктх классов из модуля(1), 
+# имена классов разделяются запятыми.
+# После импорта создается столько экземпляров класса, 
+# сколько потребуется (2) и (3).
 
+from car import Car,ElectricalCar #1
 
-from car import Car #1
+my_beetle = Car('volkswagen', 'beetle', 2019) #2
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricalCar('tesla', 'roadster', 2019)
+print(my_tesla.get_descriptive_name())
+
 
 my_new_car = Car('audi', 'a8', 2019)
 print(my_new_car.get_descriptive_name())
