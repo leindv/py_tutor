2fd03cf1b25f9a87f97553b20b503c4ee1fcb4b5
lein_d_v <leindv@yahoo.com>
2021-03-31 10:34:04 +0500

    p. 190 CLASS IMPORT file: my_car.py Ex. 3

--- my_car.py
+++ my_car.py
@@ -1,21 +1,15 @@
-# Импортирование нескольких классов из модуля.
+# Импортирование всего модуля.
 
-# Импортирование нескольктх классов из модуля(1), 
-# имена классов разделяются запятыми.
-# После импорта создается столько экземпляров класса, 
-# сколько потребуется (2) и (3).
+# В точке (1) импортируется весь модуль, после 
+# чего прога обращается к нужным классам с 
+# использованием синтаксиса:
+# ИМЯ_МОДУЛЯ.ИМЯ_КЛАССА() - точки 2 и 3.
 
-from car import Car,ElectricalCar #1
+import car #1
 
-my_beetle = Car('volkswagen', 'beetle', 2019) #2
+my_beetle = car.Car('volkswagen', 'beetle', 2019) #2
 print(my_beetle.get_descriptive_name())
 
-my_tesla = ElectricalCar('tesla', 'roadster', 2019)
+my_tesla = car.ElectricalCar('tesla', 'roadster', 2019) #3
 print(my_tesla.get_descriptive_name())
 
-
-my_new_car = Car('audi', 'a8', 2019)
-print(my_new_car.get_descriptive_name())
-
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
