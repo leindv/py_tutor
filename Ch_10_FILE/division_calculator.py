c2091512fa0e0039714a61555453783e6e7c04f1
lein_d_v <leindv@yahoo.com>
2021-04-01 16:27:17 +0500

    p. 209 file: division_calculator.py Ex. 4

--- Ch_10_FILE/division_calculator.py
+++ Ch_10_FILE/division_calculator.py
@@ -3,23 +3,34 @@
 # Использование исключений для предотвращения 
 # аварийного завершения программы.
 
-# Прога запрашивает у пользователя первое число (1).
-# Если пользователь не ввел 'q', прога запрашивает 
-# второе чило (2). Здесь также если пользователь не 
-# ввел 'q', то прого выпоняет деление первого чила 
-# на второе (3).
+# В проге кроме блока try-except есть блок else. 
+# Любой код, зависящий от успешного выполнения блока try, 
+# размещается в блоке else.
+
+# Прога пытается выполнить операцию деления в блоке (1), 
+# который включает только код, способный породить ошибку.
+# Любой код, зависящий от успешного выполнения блока try, 
+# добавляется в блок else.
+# В данном случае, если  операция деления выполняется 
+# успешно, блок else используется для вывода результата (3).
+# Блок except сообщает проге, как следует поступать при 
+# возниконовении ошибки ZeroDivisionError (2) .
 
 print('Give me two numbers, and I will divdide them.')
 print("Enter 'q' to quit.")
 
 while True:
-    first_number = input("\nFirst number: ") #1
+    first_number = input("\nFirst number: ") 
     if first_number == 'q':
         break
 
-    second_number = input("Second number: ") #2
+    second_number = input("Second number: ") 
     if second_number == 'q':
         break
 
-    answer = int(first_number) / int(second_number) #3
-    print(answer)
+    try: #1
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError: #2
+        print("You can not divide by 0!")
+    else: #3
+        print(answer)
