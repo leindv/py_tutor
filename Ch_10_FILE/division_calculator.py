aa2acd9dd35a61f3b444248e48f0cb1e7585be94
lein_d_v <leindv@yahoo.com>
2021-04-01 15:54:44 +0500

    p. 207 file: division_calculator.py Ex. 1

---
+++ Ch_10_FILE/division_calculator.py
@@ -0,0 +1,9 @@
+# Обработка исключений ZeroDivisionError.
+
+# Ошибка, упомянитая в трасеровке (1) -ZeroDivisionError - 
+# является объектом ИСКЛЮЧЕНИЯ.
+
+
+print(5/0)
+
+# ZeroDivisionError: division by zero #1
