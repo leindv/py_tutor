bdcb7dd141b8dea8b439fca9cc5265bddc31a2b0
lein_d_v <leindv@yahoo.com>
2021-04-05 15:10:08 +0500

    p. 213 file: word_count.py Ex. 3

--- Ch_10_FILE/word_count.py
+++ Ch_10_FILE/word_count.py
@@ -1,21 +1,30 @@
-# Работа с несколькими файлами.
+# Ошибки без уведомления пользователя.
 
-# Переместим основной код проги из питоновского 
-# файла предыдущего коммита в функцию с именем  
-# count_words(). При этом комментарии в проге обновили (1).
-
+# Если в предыдущем коммите мы сообщили пользователю, 
+# что три файла недоступны, то обычно  обязанность сообщать 
+# пользователю об ошибках не обязательно. 
+# Прога при возниконовении исключения должна просто 
+# проигнорировать сбой и продолжать работу.
+# Для этого блок try пишется так же как обычно, но в блоке 
+# except включаем команду pass, с которой блок ничего не 
+# делает (1).
+# В этом случае прога не выдает данных трассировки и 
+# вообще никаких результатов, указывающих на возникновение 
+# ошибки.
 
 def count_words(filename):
-    """Подсчет приблизительного количества строк в файле.""" #1
+    """Подсчет приблизительного количества строк в файле.""" 
 
     try:
         with open (filename, encoding = 'utf-8') as f:
            contents = f.read()
     except FileNotFoundError:
-        print(f"Sorry, the file {filename} does not exist.")
+        pass #1
     else:
         words = contents.split() 
         num_words = len(words) 
         print(f"The file {filename} has about {num_words} words.") 
-filename = 'alice1.txt'
-count_words(filename)
+filenames = ['alice1.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
+
