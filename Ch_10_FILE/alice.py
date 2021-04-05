da554a78054621a5642e73c53cd5a3c60662cd2f
lein_d_v <leindv@yahoo.com>
2021-04-05 14:12:15 +0500

    p. 210 file: alice.py Ex. 2

--- Ch_10_FILE/alice.py
+++ Ch_10_FILE/alice.py
@@ -1,13 +1,12 @@
 # Обработка исключения FileNotFoundError.
 
-# Файл alice.txt в рабочей директории в частности и 
-# на компе вообще не существует, используемый аргумет 
-# encoding необходим в том случае, когда кодировка 
-# нашей системы по умолчанию не совпадает с кодировкой 
-# читаемого файла.
+# С применением блока try-except.
+
 
 
 filename = 'alice.txt'
-
-with open (filename, encoding = 'uef-8') as f:
-    contents = f.read()
+try:
+    with open (filename, encoding = 'uef-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
