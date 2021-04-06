d4cce1deb5090a3ac437ae0b94f6e63410400d5a
lein_d_v <leindv@yahoo.com>
2021-04-06 15:33:15 +0500

    p. 219-220 (NB)file: remember_me.py Ex. 4

--- Ch_10_FILE/remember_me.py
+++ Ch_10_FILE/remember_me.py
@@ -4,22 +4,37 @@
 
 # С переходом на функцию комментарии дополняются строкой, 
 # которые описывают работу кода в текущей версии (1).
+# Однако функция greet_user() из предыдущего коммита не только 
+# приветсвует пользователя - она также загружает хранимое имя 
+# пользователя, если оно существует, и запрашивает новое имя, 
+# если оно не было сохранено ранее.
+# Поэтом здесь переработаем функцию greet_user().  
 
+# 
 
 import json
 
-def greet_user():
-    """Приветсвует пользователя по имени.""" #1
+def get_stored_username():
+    """Получает хранимое имя пользователя, если оно существует""" #1
     filename = 'username.json'
     try:
         with open(filename) as f: 
             username = json.load(f) 
-    except FileNotFoundError: #3
+    except FileNotFoundError: 
+        return None #2
+    else:
+        return username
+
+def greet_user():
+    """Приветствует пользователя по имени."""
+    username = get_stored_username()
+    if username: #3
+        print(f"Welcome back, {username}!")
+    else:
         username = input('What is your name? ') 
         with open(filename, 'w') as f: 
             json.dump(username, f)
             print(f"We shell remember you when you com back, {username}!")
-    else:
-        print(f"Welcom back, {username}!")
+
 greet_user()
 
