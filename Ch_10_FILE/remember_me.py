b89c3681d08563220d31e9f727ad59ba51f56c62
lein_d_v <leindv@yahoo.com>
2021-04-06 15:49:25 +0500

    p. 220 (NB)file: remember_me.py Ex. 5

--- Ch_10_FILE/remember_me.py
+++ Ch_10_FILE/remember_me.py
@@ -2,39 +2,41 @@
 # разбиения его на функции, каждая из которых решает 
 # свою конкретную задачу.
 
-# С переходом на функцию комментарии дополняются строкой, 
-# которые описывают работу кода в текущей версии (1).
-# Однако функция greet_user() из предыдущего коммита не только 
-# приветсвует пользователя - она также загружает хранимое имя 
-# пользователя, если оно существует, и запрашивает новое имя, 
-# если оно не было сохранено ранее.
-# Поэтом здесь переработаем функцию greet_user().  
+# Из функции greet_user() что в предыдущем коммите стоит вынести еще один блок.
+# Если имя пользователя не существует, то код запроса нового имени должен 
+# размещаться в функции, специализирующейся на этом решении.
 
 # 
 
 import json
 
 def get_stored_username():
-    """Получает хранимое имя пользователя, если оно существует""" #1
+    """Получает хранимое имя пользователя, если оно существует""" 
     filename = 'username.json'
     try:
         with open(filename) as f: 
             username = json.load(f) 
     except FileNotFoundError: 
-        return None #2
+        return None 
     else:
         return username
+
+def get_new_username():
+    """Запрашивает новое имя пользователя."""
+    username = input('What is your name? ') 
+    filename = 'username.json'
+    with open(filename, 'w') as f: 
+        json.dump(username, f)
+    return username
 
 def greet_user():
     """Приветствует пользователя по имени."""
     username = get_stored_username()
-    if username: #3
+    if username: 
         print(f"Welcome back, {username}!")
     else:
-        username = input('What is your name? ') 
-        with open(filename, 'w') as f: 
-            json.dump(username, f)
-            print(f"We shell remember you when you com back, {username}!")
-
+        username = get_new_username()
+        print(f"We shell remember you when you com back, {username}!")
+         
 greet_user()
 
