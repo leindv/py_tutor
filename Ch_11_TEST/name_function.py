ace04367f80716bf3e78d929092e46ed61e2d53c
lein_d_v <leindv@yahoo.com>
2021-04-12 15:58:07 +0500

    p. 226 file: name_function.py Ex. 2

--- Ch_11_TEST/name_function.py
+++ Ch_11_TEST/name_function.py
@@ -7,3 +7,17 @@ def get_formatted_name(first, middle, last):
     """Строит отформатированное полное имя."""
     full_name = f"{first} {middle} {last}"
     return full_name.title()
+
+# Результат тестирования в файле test_name_function.py
+# 1 E
+# ======================================================================
+# 2 ERROR: test_first_last_name (__main__.NamesTestCase)
+# ----------------------------------------------------------------------
+# 3 Traceback (most recent call last):
+# File "test_name_function.py", line 8, in test_first_last_name
+# formatted_name = get_formatted_name('janis', 'joplin')
+# TypeError: get_formatted_name() missing 1 required positional argument:
+# 'last'
+# ----------------------------------------------------------------------
+# 4 Ran 1 test in 0.000s
+# 5 FAILED (errors=1)
