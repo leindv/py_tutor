086b507276ecc493c0d85810ecd0ef2c479e7dce
lein_d_v <leindv@yahoo.com>
2021-04-12 16:07:50 +0500

    p. 227 file: name_function.py Ex. 3

--- Ch_11_TEST/name_function.py
+++ Ch_11_TEST/name_function.py
@@ -1,23 +1,16 @@
-# Сбой теста.
-# Изменим функцию, get_formatted_name(), чтобы она работала с отчеством
-# Первоначальная функция имеет Ex. 1
-# В этом случае файл test_name_function.py выдаст другой результат.
+# Реакция на сбой теста.
+# Так как в данном примере у функции get_formatted_name() было
+# два обязательных параметра, а теперь она требует три обязательных параметра.
+# Добавление обязательного параметра для второго имени нарушило ожидаемое
+# поведение get_formatted_name(). Здесь лучше всего сделать параметр второго
+# имени (middle) НЕОБЯЗАТЕЛЬНЫМ. Для этого параметр middle перенесем в конец
+# списка параметров и зададим ему пустое значение ро умолчанию. 
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle = ''):
     """Строит отформатированное полное имя."""
-    full_name = f"{first} {middle} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
 
-# Результат тестирования в файле test_name_function.py
-# 1 E
-# ======================================================================
-# 2 ERROR: test_first_last_name (__main__.NamesTestCase)
-# ----------------------------------------------------------------------
-# 3 Traceback (most recent call last):
-# File "test_name_function.py", line 8, in test_first_last_name
-# formatted_name = get_formatted_name('janis', 'joplin')
-# TypeError: get_formatted_name() missing 1 required positional argument:
-# 'last'
-# ----------------------------------------------------------------------
-# 4 Ran 1 test in 0.000s
-# 5 FAILED (errors=1)
