096e77fce7679d44a946246420c41f37e6c8decf
lein_d_v <leindv@yahoo.com>
2021-04-12 15:54:31 +0500

    p. 226 file: name_function.py Ex. 2

--- Ch_11_TEST/name_function.py
+++ Ch_11_TEST/name_function.py
@@ -1,5 +1,9 @@
+# Сбой теста.
+# Изменим функцию, get_formatted_name(), чтобы она работала с отчеством
+# Первоначальная функция имеет Ex. 1
+# В этом случае файл test_name_function.py выдаст другой результат.
 
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     """Строит отформатированное полное имя."""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
