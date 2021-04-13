0cf858079302f5293f1286257db794223b9d0dd3
lein_d_v <leindv@yahoo.com>
2021-04-13 15:45:56 +0500

    p. 231 file: language_survey.py

---
+++ Ch_11_TEST/language_survey.py
@@ -0,0 +1,18 @@
+from survey import AnonymousSurvey
+
+# Определение вопроса и создание экземпляра AnonymousSurvey.
+question = "What language did you first learn to speak?"
+my_survey = AnonymousSurvey(question)
+
+# Вывод вопроса и сохранение ответа.
+my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+
+# Вывод результата опроса.
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
