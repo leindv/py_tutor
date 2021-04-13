c0c8983daff63a653f5e77b761082d5ebde8610d
lein_d_v <leindv@yahoo.com>
2021-04-13 16:19:18 +0500

    p. 234 (test)file: test_survey.py Ex. 3

--- Ch_11_TEST/test_survey.py
+++ Ch_11_TEST/test_survey.py
@@ -1,32 +1,31 @@
-# Тест проверяет, что ТРИ ответа сохранились правильно.
+# Метод setUp().
 
-# Здесь определяем объект опроса по аналогии с тем, как делалось 
-# это в предыдущем коммите. Затем определяем список, содержащий три 
-# разных ответа (1) и для каждого из этих ответов вызывается метод 
-# store_response(). После того как ответы будут сохранены, следующий 
-# цикл проверяет, что каждый ответ присутсвует в my_survey.responses(2).
+# Данный метод решает две задачи: он создает экземпляр опроса(1) и
+# список ответов(2). Каждый из этих атрибутов снабжается префиксом self,
+# поэтому от может использоваться где угодно в классе.
 
 import unittest
 from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase): 
     """Тесты для класса AnonymousSurvey."""
 
+    def setUp(self):
+        """Создание опроса и набора ответов для всех тестовых методов."""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question) #1
+        self.responses = ['English', 'Spanish', 'Mandarin'] #2
+
     def test_store_single_response(self): 
         """Проверяет , что один ответ сохранен правильно."""
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question) 
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
-
+        self.my_survey.store_response(self, responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+      
     def test_store_three_responses(self):
         """Проверяет, что три ответа сохраняются праильно."""
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin'] #1
-        for response in responses:
-            my_survey.store_response(response)
-
-        for response in responses: #2
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 if __name__ == '__mine__':
     unittest.main()
